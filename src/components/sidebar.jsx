f5a4740c0ab6f1cee39b5d99db01cde2e3e17d03
Zhang <scottzhang235@gmail.com>
2017-05-06 16:40:15 -0700

    Adds sidebar

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+export default function Sidebar() {
+  return (<div className="sidebar"></div>)
+}
