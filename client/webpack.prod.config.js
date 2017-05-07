725cce424419e14bb5d4eab708fdda8f62fd42b0
Sol Chea <sol@nerdwallet.com>
2017-05-06 23:27:48 -0700

    Adding build script

---
+++ client/webpack.prod.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  devtool: 'eval-source-map',
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }, {
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader']
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
