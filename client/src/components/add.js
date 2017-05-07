7c702dadc277db7924de7824f2bbc54dfb72d07e
Ryn830 <ryn830@gmail.com>
2017-05-07 11:42:55 -0700

    (feat) Story updates while user is typing

--- client/src/components/add.js
+++ client/src/components/add.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { hacker } from 'faker'
 
 import Block from './block';
 import Engagement from './engagement';
 import Form from './form'
 import Sidebar from './sidebar';
-import { get_blocks } from '../actions/index'
+import { get_blocks, add_block } from '../actions/index'
 import Navbar from './navbar';
 
 import './add.scss';
+import './form.scss';
 
 class Add extends Component {
+  constructor () {
+    super()
+    this.state = {
+      pre: '',
+      content: '',
+      post: ''
+    }
+  }
+  updatePre (e) {
+    this.setState({
+      pre: e.target.value,
+    })
+  }
+  updateContent (e) {
+    this.setState({
+      content: e.target.value,
+    })
+  }
+  updatePost (e) {
+    this.setState({
+      post: e.target.value,
+    })
+  }
+  submit (e) {
+    e.preventDefault()
+    this.props.add_block({ pre: this.state.pre, content: this.state.content, post: this.state.post })
+    this.setState({ pre: '', content: '', post: '' })
+  }
+  generate (e) {
+    e.preventDefault()
+    let noun = hacker.noun()
+    noun = noun[0].toUpperCase() + noun.slice(1)
+    this.setState({
+      pre: hacker.phrase(),
+      content: hacker.phrase(),
+      post: noun + ' ' + hacker.verb() + '...'
+    })
+  }
   render() {
     return (
       <div className="page-container add">
@@ -26,7 +66,34 @@ class Add extends Component {
           }
         </div>
         <br></br>
-        <Form />
+        <div className='story'>
+        <div className="block-container">
+          <div className="left-bar"></div>
+          <div className="text-container">
+            { this.state.pre } { this.state.content } { this.state.post }
+          </div>
+          <div className="right-bar"></div>
+          </div>
+        </div>
+        <div className='form'>
+        <div className='left'></div>
+          <form onSubmit={ this.submit.bind(this) } className='form-group'>
+            <div>
+              <div>
+                <textarea cols="50" rows="3" value={ this.state.pre } onChange={ this.updatePre.bind(this) } placeholder='Complete the sentence from the previous story.'></textarea>
+              </div>
+              <div>
+                <textarea cols="50" rows="3" value={ this.state.content } onChange={ this.updateContent.bind(this) } placeholder='What direction do you want to move the story in?'></textarea>
+              </div>
+              <div>
+                <textarea cols="50" rows="3" value={ this.state.post } onChange={ this.updatePost.bind(this) } placeholder='Now leave a sentence to be completed.'></textarea>
+              </div>
+              <input type="submit" value="Submit" />
+              <button onClick={(e) => { this.generate(e) }}>Generate Random Responses</button>
+            </div>
+          </form>
+          <div className='right'></div>
+        </div>
       </div>
     )
   }
@@ -43,6 +110,6 @@ export default connect(
     }
   },
   (dispatch) => {
-    return bindActionCreators({ get_blocks }, dispatch)
+    return bindActionCreators({ get_blocks, add_block }, dispatch)
   }
 )(Add)
